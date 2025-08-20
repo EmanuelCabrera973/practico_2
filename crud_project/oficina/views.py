b9a885b93ec4c870f9d90c97c5ecc7322c70ee90
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-20 08:33:20 +0200

    creacion y uso de vistas genericas de operaciones CRUD

--- crud_project/oficina/views.py
+++ crud_project/oficina/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Oficina
+
+class OficinaListView(LoginRequiredMixin,ListView):
+    model=Oficina
+    paginate_by=10
+
+class OficinaCreateView(LoginRequiredMixin, CreateView):
+    model=Oficina
+    fields = ['nombre', 'nombre_corto']
+    succes_url = reverse_lazy('oficina:lista')
+    
+class OficinaUpdateView(LoginRequiredMixin, UpdateView):
+    model=Oficina
+    fields= ['nombre', 'nombre_corto']
+    succes_url = reverse_lazy('oficina:lista')
+    
+class OficinaDeleteView(LoginRequiredMixin, DeleteView):
+    model = Oficina
+    succes_url = reverse_lazy('oficina:lista')
+    
+class OficinaDeltailView(LoginRequiredMixin, DetailView):
+    model = Oficina
+    
+
 # Create your views here.
