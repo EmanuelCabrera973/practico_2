8976b513e528f4c5dd4ef384af16937a60dc054a
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-20 08:40:41 +0200

    implementacion de Personas, acceso y edad positiva

--- crud_project/persona/models.py
+++ crud_project/persona/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from oficina.models import Oficina
+
+class Persona(models.Model):
+    apellido = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    oficina = models.models.ForeignKey(Oficina, on_delete=models.CASCADE, related_name = 'personas')
+    
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
+    
+    
 
 # Create your models here.
