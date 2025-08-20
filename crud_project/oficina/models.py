239fd1947b0891b270bfd10a02047aaaeb23831c
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-20 08:17:23 +0200

    implementacion de la oficina

--- crud_project/oficina/models.py
+++ crud_project/oficina/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Oficina(models.Model):
+    nombre = models.CharField(max_Length=100)
+    nombre_corto = models.Charfield(max_length=10, unique=True)
+    
+    def __str__(self):
+        return f"{self.nombre}({self.nombre_corto})"
+    
 # Create your models here.
