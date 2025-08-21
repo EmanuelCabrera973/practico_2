29b51741b6708da180fdbfcae8f0512300259cee
EmanuelCabrera973 <emanuel.cabrera@itscordoba.edu.ar>
2025-08-21 11:15:15 +0200

    asiganacion de personas, validacion de datos y manejo de errores

--- crud_project/crud_project/urls.py
+++ crud_project/crud_project/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
-urlpatterns= 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
